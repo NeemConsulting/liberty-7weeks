2edaff1b7db129a5f8376276d3edcd45014888f5
Tommy Yau <Tommy.Yau@Unilever.com>
2020-04-27 21:52:44 +0100

    Update heroslider.js

--- studio/schemas/documents/heroslider.js
+++ studio/schemas/documents/heroslider.js
@@ -1,28 +1,27 @@
-xport default {
-    name: 'heroslider',
-    type: 'document',
-    title: 'Tool',
-    fields: [
-      {
-        name: 'heroslider',
-        type: 'string',
-        title: 'Hero xx Slider',
-      },
-      {
-        name: 'slug',
-        type: 'slug',
-        title: 'Slug',
-        options: {
-          source: 'heroslider',
-          maxLength: 96,
-        },
-      },
-      {
-        name: 'slider', 
-        type: 'array',
-        of: [{ type: 'mainImage' }],
-        title: 'Slider',
-      },
-    ],
-  }
-  
+export default {
+  name: 'heroslider',
+  type: 'document',
+  title: 'Hero Slider',
+  fields: [
+    {
+      name: 'heroslider',
+      type: 'string',
+      title: 'Hero xx Slider'
+    },
+    {
+      name: 'slug',
+      type: 'slug',
+      title: 'Slug',
+      options: {
+        source: 'heroslider',
+        maxLength: 96
+      }
+    },
+    {
+      name: 'slider',
+      type: 'array',
+      of: [{type: 'mainImage'}],
+      title: 'Slider'
+    }
+  ]
+}
