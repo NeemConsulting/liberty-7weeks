a3808ed3480d544eec5cf881ffe83ddc0582a291
Tommy Yau <Tommy.Yau@Unilever.com>
2020-05-02 00:27:04 +0100

    Update index.js

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { graphql } from "gatsby";
+import React from 'react'
+import {graphql} from 'gatsby'
 import {
   mapEdgesToNodes,
   filterOutDocsWithoutSlugs,
-  filterOutDocsPublishedInTheFuture,
-} from "../lib/helpers";
-import BlogPostPreviewList from "../components/blog-post-preview-list";
-import Container from "../components/container";
-import GraphQLErrorList from "../components/graphql-error-list";
-import SEO from "../components/seo";
-import Layout from "../containers/layout";
+  filterOutDocsPublishedInTheFuture
+} from '../lib/helpers'
+import BlogPostPreviewList from '../components/blog-post-preview-list'
+import Container from '../components/container'
+import GraphQLErrorList from '../components/graphql-error-list'
+import SEO from '../components/seo'
+import Layout from '../containers/layout'
 
 export const query = graphql`
   fragment SanityImage on SanityMainImage {
@@ -35,15 +35,15 @@ export const query = graphql`
   }
 
   query IndexPageQuery {
-    site: sanitySiteSettings(_id: { regex: "/(drafts.|)siteSettings/" }) {
+    site: sanitySiteSettings(_id: {regex: "/(drafts.|)siteSettings/"}) {
       title
       description
       keywords
     }
     posts: allSanityPost(
       limit: 6
-      sort: { fields: [publishedAt], order: DESC }
-      filter: { slug: { current: { ne: null } }, publishedAt: { ne: null } }
+      sort: {fields: [publishedAt], order: DESC}
+      filter: {slug: {current: {ne: null}}, publishedAt: {ne: null}}
     ) {
       edges {
         node {
@@ -62,30 +62,30 @@ export const query = graphql`
       }
     }
   }
-`;
+`
 
-const IndexPage = (props) => {
-  const { data, errors } = props;
+const IndexPage = props => {
+  const {data, errors} = props
 
   if (errors) {
     return (
       <Layout>
         <GraphQLErrorList errors={errors} />
       </Layout>
-    );
+    )
   }
 
-  const site = (data || {}).site;
+  const site = (data || {}).site
   const postNodes = (data || {}).posts
     ? mapEdgesToNodes(data.posts)
-        .filter(filterOutDocsWithoutSlugs)
-        .filter(filterOutDocsPublishedInTheFuture)
-    : [];
+      .filter(filterOutDocsWithoutSlugs)
+      .filter(filterOutDocsPublishedInTheFuture)
+    : []
 
   if (!site) {
     throw new Error(
       'Missing "Site settings". Open the studio at http://localhost:3333 and add some content to "Site settings" and restart the development server.'
-    );
+    )
   }
 
   return (
@@ -95,14 +95,14 @@ const IndexPage = (props) => {
         <h1 hidden>Welcome to {site.title}</h1>
         {postNodes && (
           <BlogPostPreviewList
-            title="All my blog post belong to me - NEW TEST"
+            title='All my blog post belong to me'
             nodes={postNodes}
-            browseMoreHref="/archive/"
+            browseMoreHref='/archive/'
           />
         )}
       </Container>
     </Layout>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
