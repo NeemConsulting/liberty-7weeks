a6abfc756675a80cebcb21cda3d209f606837e7e
Tommy Yau <Tommy.Yau@Unilever.com>
2020-05-02 23:32:54 +0100

    New Logo

---
+++ studio/logo/libertyLogo.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './libertyLogo.css'
+
+const libertyLogo = () => (
+  <img className={styles.libertyLogo} src='/static/liberty_logo.png' alt='Liberty Logo' />
+)
+
+export default libertyLogo
