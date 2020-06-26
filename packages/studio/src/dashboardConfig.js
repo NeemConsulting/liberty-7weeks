3ba4a06119ddad83b33d18c68514948fb3fb84d8
Rishi Dhingra <58694675+drishi1990@users.noreply.github.com>
2020-06-26 10:46:29 +0530

    Update dashboardConfig.js

--- packages/studio/src/dashboardConfig.js
+++ packages/studio/src/dashboardConfig.js
@@ -1,47 +1,8 @@
 export default {
   widgets: [
-    {name: 'structure-menu'},
-    {
-      name: 'project-info',
-      options: {
-        __experimental_before: [
-          {
-            name: 'netlify',
-            options: {
-              description:
-                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
-              sites: [
-                {
-                  buildHookId: '5e9ca69cb6c58b2c6955d569',
-                  title: 'Sanity Studio',
-                  name: 'sanity-gatsby-blog-studio-j541bj7v',
-                  apiId: '1aff24ab-1562-4aa0-bb0b-c05c123d43b0'
-                },
-                {
-                  buildHookId: '5e9ca69d8e1a5624a681f8b8',
-                  title: 'Blog Website',
-                  name: 'sanity-gatsby-blog-web-3d35kyw2',
-                  apiId: '1ce17cdb-56a3-4d9c-9862-a648edd78856'
-                }
-              ]
-            }
-          }
-        ],
-        data: [
-          {
-            title: 'GitHub repo',
-            value: 'https://github.com/tommyyau/sanity-gatsby-blog',
-            category: 'Code'
-          },
-          {
-            title: 'Frontend',
-            value: 'https://sanity-gatsby-blog-web-3d35kyw2.netlify.app',
-            category: 'apps'
-          }
-        ]
-      }
-    },
-    {name: 'project-users', layout: {height: 'auto'}},
+    { name: 'structure-menu' },
+    { name: 'deploy-github', layout: { height: 'auto' , width: 'medium'} },
+    { name: 'project-users', layout: { height: 'auto' } },
     {
       name: 'document-list',
       options: {
@@ -49,7 +10,24 @@ export default {
         order: '_createdAt desc',
         types: ['howToArticle', 'featureArticle', 'galleryArticle']
       },
-      layout: {width: 'medium'}
+      layout: { width: 'medium' }
+    },
+    {
+      name: 'project-info',
+      options: {
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'REPO_DETAILS',
+            category: 'Code'
+          },
+          {
+            title: 'Frontend',
+            value: 'FRONTEND_URL',
+            category: 'apps'
+          }
+        ]
+      }
     }
   ]
 }
